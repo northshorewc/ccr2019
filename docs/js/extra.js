ac1380bdd6f124e9cb08e481dc0bd467b9d09193
EKiefer <eric@kieferonline.com>
2020-03-17 10:17:05 -0600

    Deployed 425d485 with MkDocs version: 1.0.4

---
+++ docs/js/extra.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.your-class').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'    
+  });
+});
